ad208749012d35f64d15e77f9c81679eb4671b8b
yasinvural <yasin.vural@metu.edu.tr>
2019-03-10 15:52:30 +0300

    Updates

---
+++ precache-manifest.5a06bac972ebd282b840e6edbe8e27de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "540931996caf0318d26e",
+    "url": "/react-mealdb/static/js/runtime~main.54093199.js"
+  },
+  {
+    "revision": "4b47a360c5e640294544",
+    "url": "/react-mealdb/static/js/main.4b47a360.chunk.js"
+  },
+  {
+    "revision": "2814c52ee25a6dd88d52",
+    "url": "/react-mealdb/static/js/1.2814c52e.chunk.js"
+  },
+  {
+    "revision": "4b47a360c5e640294544",
+    "url": "/react-mealdb/static/css/main.988e6998.chunk.css"
+  },
+  {
+    "revision": "cfc3a5060fbc8d1d14e252e99912593a",
+    "url": "/react-mealdb/index.html"
+  }
+];
