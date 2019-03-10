356c77fb46bc483a586beb02c1e8446e1c5849b9
yasinvural <yasin.vural@metu.edu.tr>
2019-03-10 15:50:12 +0300

    gh-pages is installed

--- src/components/MealDetail/MealDetail.js
+++ src/components/MealDetail/MealDetail.js
@@ -1,55 +1,50 @@
-import React, { Component } from 'react';
-import './MealDetail.css';
-import MealService from '../../services/MealService';
+import React, { Component } from "react";
+import "./MealDetail.css";
+import MealService from "../../services/MealService";
 import Spinner from "../Spinner/Spinner";
 
 const mealService = new MealService();
 export default class MealDetail extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            mealDetail: []
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      mealDetail: []
+    };
+  }
 
+  componentDidMount() {
+    let id = this.props.match.params.id;
+    this.getMealDetailById(id);
+  }
+
+  async getMealDetailById(id) {
+    try {
+      let mealDetail = await mealService.getMealDetailById(id);
+      mealDetail = mealDetail.data.meals;
+
+      this.setState({
+        mealDetail
+      });
+    } catch (err) {
+      console.log(err);
     }
+  }
 
-    componentDidMount() {
-        let id = this.props.match.params.id;
-        this.getMealDetailById(id);
+  render() {
+    let { mealDetail } = this.state;
+    mealDetail = mealDetail[0];
+    if (mealDetail) {
+      return (
+        <div className="meal-detail-container">
+          <div className="meal-header">{mealDetail.strMeal}</div>
+          <div className="meal-img-container">
+            <img src={mealDetail.strMealThumb} alt="" />
+          </div>
+          <div className="meal-instruction">{mealDetail.strInstructions}</div>
+        </div>
+      );
+    } else {
+      return <Spinner />;
     }
-
-    async getMealDetailById(id) {
-        try {
-            let mealDetail = await mealService.getMealDetailById(id);
-            mealDetail = mealDetail.data.meals;
-
-            this.setState({
-                mealDetail
-            });
-        } catch (err) {
-            console.log(err);
-        }
-    }
-
-    render() {
-        let { mealDetail } = this.state;
-        mealDetail = mealDetail[0];
-        if (mealDetail) {
-            return (
-                <div className="meal-detail-container">
-                    <div className="meal-header">{mealDetail.strMeal}</div>
-                    <div className="meal-img-container">
-                        <img src={mealDetail.strMealThumb} alt="" />
-                    </div>
-                    <div className="meal-instruction">
-                        {mealDetail.strInstructions}
-                    </div>
-                </div>
-            )
-        } else {
-            return (
-                <Spinner/>
-            )
-        }
-    }
+  }
 }
